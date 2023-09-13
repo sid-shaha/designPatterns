18db296a935f715a6904cc8c0ea57e5249569217
sid-shaha <siddhantshaha28@gmail.com>
2023-09-13 20:20:26 +0530

    feat: Added Singleton Class

---
+++ src/Singleton/Logger.java
@@ -0,0 +1,15 @@
+package Singleton;
+
+public class Logger {
+
+    private static Logger logger;
+    private Logger(){
+        System.out.println("Logger Instance Created");
+    }
+    public static Logger getLogger(){
+        if(logger == null){
+            logger = new Logger();
+        }
+        return logger;
+    }
+}
---
+++ src/Singleton/Main.java
@@ -0,0 +1,11 @@
+package Singleton;
+
+public class Main {
+
+    public static void main(String[] args){
+        Logger logger = Logger.getLogger();
+        Logger logger2 = Logger.getLogger();
+        Logger logger3 = Logger.getLogger();
+    }
+
+}
